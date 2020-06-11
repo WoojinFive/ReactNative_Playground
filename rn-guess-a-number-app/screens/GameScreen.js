57e95d62a93cc60be7df6263407dd4f5e2ba73ad
woojinfive <kh3996@gmail.com>
2020-06-10 22:42:46 -0300

    custom button

--- rn-guess-a-number-app/screens/GameScreen.js
+++ rn-guess-a-number-app/screens/GameScreen.js
@@ -3,6 +3,7 @@ import { View, Text, StyleSheet, Button, Alert } from 'react-native';
 
 import NumberContainer from '../components/NumberContainer';
 import Card from '../components/Card';
+import MainButton from '../components/MainButton';
 import DefaultStyle from '../constants/default-styles';
 
 const generateRandomBetween = (min, max, exclude) => {
@@ -64,16 +65,14 @@ const GameScreen = (props) => {
       <NumberContainer>{currentGuess}</NumberContainer>
       <Card style={styles.buttonContainer}>
         <View>
-          <Button
-            title='LOWER'
-            onPress={nextGuessHandler.bind(this, 'lower')}
-          />
+          <MainButton onPress={nextGuessHandler.bind(this, 'lower')}>
+            LOWER
+          </MainButton>
         </View>
         <View>
-          <Button
-            title='GREATER'
-            onPress={nextGuessHandler.bind(this, 'greater')}
-          />
+          <MainButton onPress={nextGuessHandler.bind(this, 'greater')}>
+            GREATER
+          </MainButton>
         </View>
       </Card>
     </View>
@@ -90,8 +89,8 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-around',
     marginTop: 20,
-    width: 300,
-    maxWidth: '80%',
+    width: 400,
+    maxWidth: '90%',
   },
 });
 
