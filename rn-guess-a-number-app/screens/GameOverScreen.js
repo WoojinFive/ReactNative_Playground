d0326c4f6ebee0a6af9478df5ad87d79be93528e
woojinfive <kh3996@gmail.com>
2020-06-11 22:13:24 -0300

    listening to orientation changes

--- rn-guess-a-number-app/screens/GameOverScreen.js
+++ rn-guess-a-number-app/screens/GameOverScreen.js
@@ -6,6 +6,7 @@ import {
   Button,
   Image,
   Dimensions,
+  ScrollView,
 } from 'react-native';
 
 import BodyText from '../components/BodyText';
@@ -15,30 +16,32 @@ import Colors from '../constants/colors';
 
 const GameOverScreen = (props) => {
   return (
-    <View style={styles.screen}>
-      <TitleText>The Game is Over!</TitleText>
-      <View style={styles.imageContainer}>
-        <Image
-          fadeDuration={300} // default value is 300
-          source={require('../assets/success.png')}
-          // source={{
-          //   uri:
-          //     'https://cdn.pixabay.com/photo/2016/05/05/23/52/mountain-summit-1375015_960_720.jpg',
-          // }}
-          style={styles.image}
-          resizeMode='cover'
-        />
+    <ScrollView>
+      <View style={styles.screen}>
+        <TitleText>The Game is Over!</TitleText>
+        <View style={styles.imageContainer}>
+          <Image
+            fadeDuration={300} // default value is 300
+            source={require('../assets/success.png')}
+            // source={{
+            //   uri:
+            //     'https://cdn.pixabay.com/photo/2016/05/05/23/52/mountain-summit-1375015_960_720.jpg',
+            // }}
+            style={styles.image}
+            resizeMode='cover'
+          />
+        </View>
+        <View style={styles.resultContainer}>
+          <BodyText style={styles.resultText}>
+            Your phone needed{' '}
+            <Text style={styles.highlight}>{props.roundsNumber}</Text> rounds to
+            guess the number{' '}
+            <Text style={styles.highlight}>{props.userNumber}</Text>
+          </BodyText>
+        </View>
+        <MainButton onPress={props.onRestart}>NEW GAME</MainButton>
       </View>
-      <View style={styles.resultContainer}>
-        <BodyText style={styles.resultText}>
-          Your phone needed{' '}
-          <Text style={styles.highlight}>{props.roundsNumber}</Text> rounds to
-          guess the number{' '}
-          <Text style={styles.highlight}>{props.userNumber}</Text>
-        </BodyText>
-      </View>
-      <MainButton onPress={props.onRestart}>NEW GAME</MainButton>
-    </View>
+    </ScrollView>
   );
 };
 
